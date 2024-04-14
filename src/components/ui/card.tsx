583dd13cacb43c53b6b3c286789a8d03d90b540d
NoNonOno NoNonOno <devliveforever@gmail.com>
2024-04-14 10:18:40 -0400

    updates cards with json

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -47,7 +47,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={cn("text-primary text-base font-normal", className)}
     {...props}
   />
 ))
